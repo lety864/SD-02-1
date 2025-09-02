48d361aaa607c14c8ca2717f8c10e549d6117544
Leticia Janet BR <bermudez.leticia.2609@gmail.com>
2025-09-01 19:38:40 -0600

    update se uso el modulo para 3 y 5

--- task3.js
+++ task3.js
@@ -1,5 +1,16 @@
 // Refer to Task 3 in your Instructions to complete this task
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Three!");
-  };
+for (let i = 1; i <= 105; i++) {
+  
+  if(i%3 === 0 && i%5 == 0){
+    console.log("FizzBuzz");
+  }else if(i % 3 === 0){
+    console.log("Fizz");
+  }else if(i % 5 === 0){
+    console.log("Buzz");
+  }else{
+    console.log(i);
+  }
+};
+
+
