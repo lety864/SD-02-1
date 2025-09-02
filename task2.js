9081934e842d30ef769240b378846be726a67197
Leticia Janet BR <bermudez.leticia.2609@gmail.com>
2025-09-01 19:37:49 -0600

    update se agrego el uso del modulo para valores divisbles entre 3

--- task2.js
+++ task2.js
@@ -1,5 +1,9 @@
 // Refer to Task 2 in your Instructions to complete this task
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Two!");
-  };
+for (let i = 1; i <= 105; i++) {
+  if(i % 3 === 0){
+    console.log("Fizz");
+  }else{
+    console.log(i);
+  }
+};
