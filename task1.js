41ade24b9ccdcee846ff77a865327b5b5faad79a
Leticia Janet BR <bermudez.leticia.2609@gmail.com>
2025-09-01 19:36:12 -0600

    update se agrego un console log para imprimir 105 datos

--- task1.js
+++ task1.js
@@ -1,5 +1,5 @@
 // Refer to Task 1 in your Instructions to complete this task
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task One!");
-  };
+for (let i = 1; i <= 105; i++) {
+  console.log(i);
+};
