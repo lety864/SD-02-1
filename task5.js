d71191ca61d4ce28b5bcba0b7b7b773bd65c3fab
Leticia Janet BR <bermudez.leticia.2609@gmail.com>
2025-09-01 19:40:28 -0600

    update se hizo uso del prompt de nodejs

--- task5.js
+++ task5.js
@@ -1,5 +1,25 @@
 // Refer to Task 5 in your Instructions to complete this task
+const prompt = require("prompt-sync")();
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Five!");
-  };
+const limit = +prompt("Ingresa hasta que numero quieres generar el FizzBuzz: ");
+
+for (let i = 1; i <= limit; i++) {
+  
+  if(i%3 === 0 && i%5 == 0 && i%7 ==0){
+    console.log("FizzBuzzWoof");
+  }else if(i%3 === 0 && i%7 == 0){
+    console.log("FizzWoof");
+  }else if(i%5 === 0 && i%7 == 0){
+    console.log("BuzzWoof");
+  }else if(i%3 === 0 && i%5 == 0){
+    console.log("FizzBuzz");
+  }else if(i % 3 === 0){
+    console.log("Fizz");
+  }else if(i % 5 === 0){
+    console.log("Buzz");
+  }else if(i % 7 === 0){
+    console.log("Woof");
+  }else{
+    console.log(i);
+  }
+};
