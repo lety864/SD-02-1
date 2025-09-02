54944e53fdf621bdc41a61fb4eada0c0637a3c78
Leticia Janet BR <bermudez.leticia.2609@gmail.com>
2025-09-01 19:39:38 -0600

    update se agrego el modulo para 7

--- task4.js
+++ task4.js
@@ -1,5 +1,21 @@
 // Refer to Task 4 in your Instructions to complete this task
-
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Four!");
-  };
+for (let i = 1; i <= 105; i++) {
+  
+  if(i%3 === 0 && i%5 == 0 && i%7 ==0){
+    console.log("FizzBuzzWoof");
+  }else if(i%3 === 0 && i%7 == 0){
+    console.log("FizzWoof");
+  }else if(i%5 === 0 && i%7 == 0){
+    console.log("BuzzWoof");
+  }else if(i%3 === 0 && i%5 == 0){
+    console.log("FizzBuzz");
+  }else if(i % 3 === 0){
+    console.log("Fizz");
+  }else if(i % 5 === 0){
+    console.log("Buzz");
+  }else if(i % 7 === 0){
+    console.log("Woof");
+  }else{
+    console.log(i);
+  }
+};
